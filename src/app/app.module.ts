04510eb7d6c103abb0fa98e756e86817940994f3
Crauseo26 <carlos.rauseo@innovant.us>
2022-03-18 20:02:52 -0300

    [Entrega 1]: componentes agular

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [AppComponent, ToolbarComponent, NavbarComponent, ContentComponent, FooterComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
